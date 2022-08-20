5cf97c6b62e6fddbcf680f93c8c940b35cbfdf0f
Jose francisco Arredondo amador <jarredon@c3r3s1.42malaga.com>
2022-08-20 12:57:27 +0200

    check vulnerabilities in 3 webs

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 dvwa:
-	docker run -it -d -p 80:80 --name dvwa vulnerables/web-dvwa
+	docker run -it -d --name dvwa -p 80:80 vulnerables/web-dvwa
 
 goat:
-	docker run -d -p 8080:8080 -p 9090:9090 -p 8000:8888 -e TZ=Europe/Madrid webgoat/goatandwolf:latest
+	docker run -d --name goat -p 8080:8080 -p 9090:9090 -p 8000:8888 -e TZ=Europe/Madrid webgoat/goatandwolf:latest
+
+simple:
+	docker run -d --name simple -p 1234:8000 -it appsecco/dsvw
 
 stop:
 	-docker stop $$(docker ps -q)
+
+prune:
+	docker container prune
 
 fclean: stop
 	docker rmi $$(docker image ls -q)
--- vaccine.py
+++ vaccine.py
@@ -18,10 +18,26 @@ def parse_cookies(string):
         name, value = c.split("=")
         s.cookies.set(name=name, value=value)
 
+def parse_data(string):
+    if string == None:
+        return None
+    data = {}
+    string = string.split("&")
+    for d in string:
+        if d.count("=") != 1:
+            print("Error parsing data")
+            quit()
+        key, value = d.split("=")
+        data[key] = value
+    return data
 
 def get_all_forms(url):
     """Returns all forms from the HTML content of a url"""
-    soup = bs(s.get(url).content, "html.parser")
+    try:
+        soup = bs(s.get(url).content, "html.parser")
+    except Exception as e:
+        print(e)
+        quit()
     return soup.find_all("form")
 
 def get_form_details(form):
@@ -55,6 +71,10 @@ def is_vulnerable(response):
         "unclosed quotation mark after the character string",
         # Oracle
         "quoted string not properly terminated",
+        # MariaDB
+        "malformed",
+        # Python
+        "unrecognized token"
     }
     for error in errors:
         if error in response.content.decode().lower():
@@ -62,6 +82,7 @@ def is_vulnerable(response):
     return False
 
 def check_form(url):
+    vulnerable = False
     forms = get_all_forms(url)
     for form in forms:
         form_details = get_form_details(form)
@@ -84,9 +105,32 @@ def check_form(url):
                 res = s.get(url, params=data)
             # Check response
             if is_vulnerable(res):
-                print("\nSQL Injection vulnerability detected in:\n\t", url)
+                vulnerable = True
+                print("\nSQL Injection vulnerability detected in:\n\t", res.url)
                 print("data:")
                 pprint(data)
+    if not vulnerable:
+        print(url, "\n\tdoesn't seem to be vulnerable")
+
+def check_url(url, data, method):
+    if data == None:
+        print("No data to process. Use '--data' argument")
+        quit()
+    for c in "\"'":
+        for k in data:
+            data[k] = f"test{c}"
+        try:
+            if method == "GET":
+                res = s.get(url, params=data)
+            elif method == "POST":
+                res = s.post(url, data=data)
+        except Exception as e:
+            print(e)
+            quit()
+        if is_vulnerable(res):
+            print("url vulnerable")
+        else:
+            print("url not vulnerable")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -96,13 +140,25 @@ def main():
         help="Type of request, if not specified GET will be used")
     parser.add_argument("url", help="url to scan")
     parser.add_argument("-c", "--cookie", 
-            help="introduce cookies in form of 'name=value&name=value...'")
+            help="introduce cookies in the form of 'name=value&name=value...'")
+    parser.add_argument("-d", "--data",
+            help="introduce data to submit in the form of 'key=value&key=...'")
+    parser.add_argument("-f", "--forms", action="store_true",
+            help="look for forms in the url to inject SQL")
     args = parser.parse_args()
+
+    if (args.X != "GET" and args.X != "POST"):
+        print("The method should be only 'GET' or 'POST'")
+        quit()
 
     if (args.cookie):
         parse_cookies(args.cookie)
 
-    check_form(args.url)
+    if (args.forms):
+        check_form(args.url)
+    else:
+        check_url(args.url, parse_data(args.data), args.X)
+
 
 if __name__ == "__main__":
     main()
